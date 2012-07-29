7cf2114a6dcad4b17395f1438467d02e5e4d41c6
rshanrath <scotthanrath@gmail.com>
2012-07-29 15:56:29 -0500

    Now checks for publisher policies based on journal title if ISSN is not present or doesn't return a match.  Added optional use of a SherpaRomeo API key.  Support fo multiple publishers in SherpaRomeo responses.

--- SherpaRomeo.php
+++ SherpaRomeo.php
@@ -1,13 +1,19 @@
 <?php
 require_once('Zend/Http/Client.php');
+require_once('Publisher.php');
+
 
 class SherpaRomeo {
-  private $pre;
-  private $post;
-  private $pdf;
+  private $issn;
+  private $jtitle;
+  private $publishers = array();
+  private $num_hits = 0;
 
-  function __construct($query, $request_type = 'issn'){
-    $query_string = 'versions=all&'.$request_type.'='.$query;
+  function __construct($query, $request_type = 'issn', $sr_api_key = ''){
+    $query_string = 'versions=all&'.$request_type.'='.urlencode($query);
+    if ($sr_api_key){
+      $query_string .= '&ak='.$sr_api_key;
+    }
     $client = new Zend_HTTP_Client();
     $client->setUri('http://www.sherpa.ac.uk/romeo/api29.php?'.$query_string);
     $response = $client->request();
@@ -16,22 +22,67 @@ class SherpaRomeo {
       $sr = $response->getBody();
       $sr_xml = new DOMDocument();
       $sr_xml->loadXML($sr);
-      $this->pre = $sr_xml->getElementsByTagName('prearchiving')->item(0)->nodeValue;
-      $this->post = $sr_xml->getElementsByTagName('postarchiving')->item(0)->nodeValue;
-      $this->pdf = $sr_xml->getElementsByTagName('pdfarchiving')->item(0)->nodeValue;
+      $this->num_hits = $sr_xml->getElementsByTagName('numhits')->item(0)->nodeValue;
+      $this->issn = $sr_xml->getElementsByTagName('issn')->item(0)->nodeValue;
+      $this->jtitle = $sr_xml->getElementsByTagName('jtitle')->item(0)->nodeValue;
+
+      $pubs = $sr_xml->getElementsByTagName('publisher');
+      for ($i = 0; $i < $pubs->length; $i++){
+        $id = $pubs->item($i)->getAttribute('id');
+        $name = $pubs->item($i)->getElementsByTagName('name')->item(0)->nodeValue;
+
+        $p = new Publisher($id, $name);
+        $p->setPrearchiving($pubs->item($i)->getElementsByTagName('prearchiving')->item(0)->nodeValue);
+        $p->setPostarchiving($pubs->item($i)->getElementsByTagName('postarchiving')->item(0)->nodeValue);
+        $p->setPdfarchiving($pubs->item($i)->getElementsByTagName('pdfarchiving')->item(0)->nodeValue);
+
+        $prerestrictions_xml = $pubs->item($i)->getElementsByTagName('prerestriction');
+        $prerestrictions = array();
+        for ($j = 0; $j < $prerestrictions_xml->length; $j++){
+          $prerestrictions[] = strip_tags(trim($prerestrictions_xml->item($j)->nodeValue));
+        }
+        $p->setPrerestrictions($prerestrictions);
+
+        $postrestrictions_xml = $pubs->item($i)->getElementsByTagName('postrestriction');
+        $postrestrictions = array();
+        for ($j = 0; $j < $postrestrictions_xml->length; $j++){
+          $postrestrictions[] = strip_tags(trim($postrestrictions_xml->item($j)->textContent));
+        }
+        $p->setPostrestrictions($postrestrictions);
+
+        $pdfrestrictions_xml = $pubs->item($i)->getElementsByTagName('pdfrestriction');
+        $pdfrestrictions = array();
+        for ($j = 0; $j < $pdfrestrictions_xml->length; $j++){
+          $pdfrestrictions[] = strip_tags(trim($pdfrestrictions_xml->item($j)->nodeValue));
+        }
+        $p->setPdfrestrictions($pdfrestrictions);
+
+        $conditions_xml = $pubs->item($i)->getElementsByTagName('condition');
+        $conditions = array();
+        for ($j = 0; $j < $conditions_xml->length; $j++){
+          $conditions[] = $conditions_xml->item($j)->nodeValue;
+        }
+        $p->setConditions($conditions);
+
+        $this->publishers[$id] = $p;
+      }
     }
   }
-
-  public function getPreArchiving(){
-    return $this->pre;
+  
+  public function getPublishers() {
+    return $this->publishers;
   }
 
-  public function getPostArchiving(){
-    return $this->post;
+  public function setPublishers($publishers) {
+    $this->publishers = $publishers;
   }
 
-  public function getPdfArchiving(){
-    return $this->pdf;
+  public function getIssn(){
+    return $this->issn;
+  }
+
+  public function getNumHits(){
+    return $this->num_hits;
   }
 }
 
--- zotero_sherpa.php
+++ zotero_sherpa.php
@@ -1,5 +1,5 @@
 <?php
-#set_include_path(get_include_path() . PATH_SEPARATOR . '/path/to/Zend/library');
+//set_include_path(get_include_path() . PATH_SEPARATOR . '/path/to/Zend/library');
 require_once('Zend/Http/Client.php');
 require_once('SherpaRomeo.php');
 
@@ -10,11 +10,12 @@ if ((! isset($argv[1])) || (! isset($argv[2]))){
 }
 
 $userid = $argv[1];
-$key = $argv[2];
+$zotero_key = $argv[2];
+$sherpa_romeo_key = $argv[3];
 
 $url_base = 'https://api.zotero.org/users/'.$userid.'/';
 $client = new Zend_HTTP_Client();
-$client->setUri($url_base.'items/?key='.$key.'&itemType=journalArticle&content=json');
+$client->setUri($url_base.'items/?key='.$zotero_key.'&itemType=journalArticle&content=json');
 $response = $client->request();
 
 if (200 != $response->getStatus()){
@@ -37,38 +38,58 @@ foreach ($item_list as $item){
 
   $item_key = $item->getElementsByTagNameNS('http://zotero.org/ns/api', 'key')->item(0)->nodeValue;
   $issn = $content_json->ISSN;
+  $jtitle = $content_json->publicationTitle;
  
-  if ($issn){
-    if (!isset($sr_responses[$issn])){
-      $sr_data = new SherpaRomeo($issn);
-      $sr_responses[$issn] = $sr_data;
+  if ($issn || $jtitle){
+    $sr_key = str_replace(" ", "", $issn.$jtitle);
+    if (!isset($sr_responses[$sr_key])){
+      $issn_hits = 0;
+      if ($issn){
+        $sr_data = new SherpaRomeo($issn, 'issn', $sherpa_romeo_key);
+        if ($sr_data->getNumHits() > 0){
+          $issn_hits = $sr_data->getNumHits();
+          $sr_responses[$sr_key] = $sr_data;
+        }
+      }
+      if ($issn_hits == 0){
+        if ($jtitle){
+          $sr_data = new SherpaRomeo($jtitle, 'jtitle', $sherpa_romeo_key);
+          $sr_responses[$sr_key] = $sr_data;
+        }
+      }
     }else{
-      $sr_data = $sr_responses[$issn];
+      $sr_data = $sr_responses[$sr_key];
     }
 
-    $content_json->tags[] = array('tag' => 'SherpaRomeo Pre '.$sr_data->getPreArchiving(), 'type' => 1);
-    $content_json->tags[] = array('tag' => 'SherpaRomeo Post '.$sr_data->getPreArchiving(), 'type' => 1); 
-    $content_json->tags[] = array('tag' => 'SherpaRomeo PDF '.$sr_data->getPdfArchiving(), 'type' => 1); 
+    $pubs = array_values($sr_data->getPublishers());
+    if (count($pubs) == 1){
+      $pub = $pubs[0];
+      $content_json->tags[] = array('tag' => 'SherpaRomeo Pre '.$pub->getPreArchiving(), 'type' => 1);
+      $content_json->tags[] = array('tag' => 'SherpaRomeo Post '.$pub->getPreArchiving(), 'type' => 1); 
+      $content_json->tags[] = array('tag' => 'SherpaRomeo PDF '.$pub->getPdfArchiving(), 'type' => 1); 
 
-    $client->setUri($url_base.'items/'.$item_key.'?key='.$key);
-    $client->setHeaders('Content-Type', 'application/json');
-    $client->setHeaders('If-Match', $etag);
-    $client->setRawData(json_encode($content_json));
-    $update_response = $client->request('PUT');
+      $client->setUri($url_base.'items/'.$item_key.'?key='.$zotero_key);
+      $client->setHeaders('Content-Type', 'application/json');
+      $client->setHeaders('If-Match', $etag);
+      $client->setRawData(json_encode($content_json));
+      $update_response = $client->request('PUT');
 
-    if (200 == $update_response->getStatus()){
-      echo "Update successful for ".$item_key."\n";
+      if (200 == $update_response->getStatus()){
+        echo "Update successful for ".$item_key."\n";
+      }else{
+        echo "Update failed (".$update_response->getStatus().") for ".$item_key."\n";
+      }
     }else{
-      echo "Update failed (".$update_response->getStatus().") for ".$item_key."\n";
+      echo "Couldn't find single publisher for ".$item_key."\n";
     }
   }else{
-    echo "No ISSN for ".$item_key."\n"; 
+    echo "No ISSN or publicationTitle for ".$item_key."\n";
   }
 }
 
 function usage(){
   echo "\n";
-  echo "zotero_sherpa.pbp [zotero_user] [zotero_key]";
+  echo "zotero_sherpa.pbp [zotero_user] [zotero_key] [sherpa_romeo_key]";
   echo "\n";
 }
 
